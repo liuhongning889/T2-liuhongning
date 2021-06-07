e4e79db9983081153b96c9dd7afd5ec49288740e
liuhongning889 <85432747+liuhongning889@users.noreply.github.com>
2021-06-07 08:58:14 +0800

    python 的作业答案

--- python_homework.py
+++ python_homework.py
@@ -11,8 +11,14 @@ user_agent_list = [
         "(KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
     ]
 headers = {
+        {
+        'Proxy-Connection' : 'keep-alive',
+        'Accept':'application/json, text/plain, */*',
         'User-Agent': random.choice(user_agent_list),
-        "'Referer": "http://scm.xesv5.com/"
+        'Referer': 'http://scm.xesv5.com/',
+        'token' : '1832b85ff6bc7d1a321cd27ff9d4abc1',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cookie' :'Sign=f6c9d31c879d889a737f380c28d07ae4; From-User=xuezhenhua; UM_distinctid=1766a97c004b3a-05008f93baef8f-c791039-144000-1766a97c005bd6; token=eyJhcHBpZCI6IjExMyIsInRva2VuIjoiYTkwYWIzNWZmZmQ1NWM0OTM0ZDlkYjRmZGNmNjYzYjkifQ%3D%3D; pmaUser=%7B%22iv%22%3A%22vgd%2BkishMyrpiZ8KQsMAnQ%3D%3D%22%2C%22mac%22%3A%228d428e35fa0de12a78d3e5390c8f0d594efab774%22%2C%22payload%22%3A%22r6%2Bs7bRcCSJGR3NWrZkm%5C%2Fw%3D%3D%22%7D; pmaAuth=%7B%22iv%22%3A%22%5C%2Fgv8BiLMKwDggUMejuKHOA%3D%3D%22%2C%22mac%22%3A%222915b4ccf8feb0d99163cef01bc00fc2d9678c85%22%2C%22payload%22%3A%22Dv7dBs5r8Ici6fryJBANkVF2g2B91k0Z4I3qNI%5C%2FZmMA%3D%22%7D; wx=d74f2fc6f78f28cdf09dfb6390e1548709yfybbsfm'
     }
 def getData(url):
     try:
